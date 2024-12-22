0da9d1a8d2e6d3c7855bccf02bfb99f7ac95d77c
krino45 <krina645@gmail.com>
2024-12-22 23:20:14 +0700

    Add infinite thing support thing

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <csignal>
+#include <thread>
+#include <chrono>
+
+volatile bool running = true;
+
+void signalHandler(int signum) {
+    std::cout << "\nЗавершение работы. (сигнал " << signum << ")" << std::endl;
+    running = false;
+}
 
 int main () { 
-	for (int i = 1; i < 11; i+=2)
+    std::signal(SIGINT, signalHandler);
+    std::signal(SIGTERM, signalHandler);
+
+    while (running) {
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        std::cout << "Приложение работает..." << std::endl;
+        for (int i = 1; i < 11; i+=2)
 	{
 		for (int j = 0; j < (11 - i) / 2; j++) std::cout << " ";
 		std::cout << std::endl;
@@ -10,5 +26,7 @@ int main () {
 		for (int j = 0; j < (11 - i) / 2; j++) std::cout << " ";
 		std::cout << std::endl;
 	}
-	return 0;
+    }
+    std::cout << "Приложение завершено." << std::endl;
+    return 0;
 }
