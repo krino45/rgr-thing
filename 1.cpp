ae07ad7eb9983cab740038ddcacbadcf7ff71308
krino45 <krina645@gmail.com>
2024-12-23 14:14:05 +0700

    Remove endline

--- 1.cpp
+++ 1.cpp
@@ -20,9 +20,7 @@ int main () {
         for (int i = 1; i < 11; i+=2)
 	{
 		for (int j = 0; j < (11 - i) / 2; j++) std::cout << " ";
-		std::cout << std::endl;
 		for (int j = 0; j < i; j++) std::cout << "#";
-		std::cout << std::endl;
 		for (int j = 0; j < (11 - i) / 2; j++) std::cout << " ";
 		std::cout << std::endl;
 	}
